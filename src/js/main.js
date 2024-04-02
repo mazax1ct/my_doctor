9420ad294a0204bf3b7429ed27fe4f5d7b84cb81
mazax1ct <vova@giveandget.ru>
2024-04-02 17:39:29 +0300

    footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
+//отслеживание скролла для шапки
+var header = $('.header'),
+    scrollPrev = 0;
+
+var resize_scroll = function(e) {
+  var scrolled = $(window).scrollTop();
+
+  if (scrolled > $('.header').height() * 2) {
+		header.addClass('is-scrolled');
+	} else {
+		header.removeClass('is-scrolled');
+	}
+
+  if ( scrolled > $('.header').height() && scrolled > scrollPrev ) {
+		header.addClass('is-out');
+	} else {
+		header.removeClass('is-out');
+	}
+
+	scrollPrev = scrolled;
+};
+
+$(document).ready(function() {
+  //запуск функции навешивания класса на шапку
+  resize_scroll();
+});
+
+//перезапуск функции навешивания класса на шапку при скролле и ресайзе
+$(window).on("scroll", resize_scroll).on("resize", resize_scroll);
+
 //тогглер меню
 $(document).on('click', '.js-menu-toggler', function () {
   if(!$(this).hasClass('is-active')) {
